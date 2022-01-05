6d2a820e43b057aec54c9115ddc550f72886a878
Rafał Kazik <kazik.raf@gmail.com>
2022-01-05 19:55:40 +0100

    js implementation v2

--- src/js/app.js
+++ src/js/app.js
@@ -36,16 +36,70 @@ function loadUserRepos() {
     })
     .then((data) => {
       usernameInput.value = "";
-      console.log(data);
+      usernameInput.classList.remove("form__input--username-error");
+      insertUserRepos(data);
+      loadUserAvatar(data);
+      loadUserName(data);
     })
     .catch((err) => {
       usernameInput.value = "";
-      alert("blad!");
+      usernameInput.classList.add("form__input--username-error");
     });
 }
 
-function loadUserAvatar() {
-  console.log("av_change");
+function insertUserRepos(reposArr) {
+  const reposList = document.querySelector(".repositories__list");
+  const reposListElement = document.querySelector(".list__element");
+
+  reposList.innerHTML = "";
+
+  return reposArr.forEach((item) => {
+    const cloneReposListElement = reposListElement.cloneNode(true);
+    cloneReposListElement.classList.remove("list__element--prototype");
+
+    function setNameOfRepository() {
+      const nameOfRepository = cloneReposListElement.querySelector(
+        ".element-repo-name__title"
+      );
+      return (nameOfRepository.innerText = item.full_name);
+    }
+    setNameOfRepository();
+
+    function setIdOfRepository() {
+      const idOfRepository = cloneReposListElement.querySelector(".id__number");
+      return (idOfRepository.innerText = item.id);
+    }
+    setIdOfRepository();
+
+    function setDescriptionOfRepository() {
+      const descOfRepository = cloneReposListElement.querySelector(
+        ".element-repo-desc__text"
+      );
+      if (item.description) {
+        return (descOfRepository.innerText = item.description);
+      } else {
+        return (descOfRepository.innerText = `No description`);
+      }
+    }
+    setDescriptionOfRepository();
+
+    reposList.appendChild(cloneReposListElement);
+  });
+}
+
+function loadUserAvatar(reposAv) {
+  const avatarUrl = reposAv[0]["owner"]["avatar_url"];
+  const userAvatar = document
+    .querySelector(".user__img")
+    .setAttribute("src", avatarUrl);
+  userAvatar;
+}
+
+function loadUserName(usernameOfRepos) {
+  const login = usernameOfRepos[0]["owner"]["login"];
+  const userName = (document.querySelector(
+    ".user__name"
+  ).innerText = `@${login}`);
 }
 
 form.addEventListener("submit", init);
